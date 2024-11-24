75a14f8eb7cd1e8d6325765c28d3286786af73f4
Robwam <charlie@begly.co.uk>
2024-11-24 15:30:05 +0000

    Add game state and grid infra

---
+++ Snake/GameState.cs
@@ -0,0 +1,149 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Snake
+{
+    class GameState
+    {
+        private readonly int _defaultSnakeLength = 3;
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            if (cols <= _defaultSnakeLength)
+            {
+                throw new ArgumentOutOfRangeException(nameof(cols), $"The number of columns must be greater than the snake's default length: {_defaultSnakeLength}");
+            }
+            
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[Rows, Cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+            AddFood();
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+            
+            for (int c = 1; c <= _defaultSnakeLength; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+            }
+        }
+
+        private IEnumerable<Position> EmptyPositions()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Cols; c++)
+                {
+                    if (Grid[r,c] == GridValue.Emtpy)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+
+        private void AddFood()
+        {
+            var emtpyPositions = new List<Position>(EmptyPositions());
+            
+            if (emtpyPositions.Count == 0)
+            {
+                return;
+            }
+
+            var pos = emtpyPositions[random.Next(emtpyPositions.Count)];
+            Grid[pos.Row, pos.Col] = GridValue.Food;
+        }
+
+        public Position HeadPosition()
+        {
+            return snakePositions.First.Value;
+        }
+
+        public Position TailPosition()
+        {
+            return snakePositions.Last.Value;
+        }
+
+        public IEnumerable<Position> SnakePositions()
+        {
+            return snakePositions;
+        }
+
+        private void AddHead(Position pos)
+        {
+            snakePositions.AddFirst(pos);
+            Grid[pos.Row, pos.Col] = GridValue.Snake;
+        }
+
+        private void RemoveTail()
+        {
+            var tail = snakePositions.Last.Value;
+            snakePositions.RemoveLast();
+            Grid[tail.Row, tail.Col] = GridValue.Emtpy;
+        }
+
+        public void ChangeDirection(Direction dir)
+        {
+            Dir = dir;
+        }
+
+        private bool OutsideGrid(Position pos)
+        {
+            return pos.Row < 0 || pos.Col < 0 || pos.Row >= Rows || pos.Col >= Cols;
+        }
+
+        private GridValue WillHit(Position newHeadPos) // What the snake will hit next if it moves there
+        {
+            if (OutsideGrid(newHeadPos))
+            {
+                return GridValue.Outside;
+            }
+
+            if (newHeadPos == TailPosition()) // Head will not collide with tail as tail would move out of way
+            {
+                return GridValue.Emtpy;
+            }
+
+            return Grid[newHeadPos.Row, newHeadPos.Col];
+        }
+
+        public void Move()
+        {
+            var newHeadPos = HeadPosition().Translate(Dir);
+            GridValue hit = WillHit(newHeadPos);
+
+            if (hit == GridValue.Outside || hit == GridValue.Snake)
+            {
+                GameOver = true;
+            }
+            else if (hit == GridValue.Emtpy)
+            {
+                RemoveTail();
+                AddHead(newHeadPos);
+            }
+            else if (hit == GridValue.Food)
+            {
+                AddHead(newHeadPos);
+                Score++;
+                AddFood();
+            }
+
+        }
+    }
+}
